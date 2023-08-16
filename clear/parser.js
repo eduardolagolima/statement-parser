61bdde003619609820e218505e094cc6faf013d1
eduardolagolima <eduardolagolima@gmail.com>
2023-08-16 14:47:43 -0300

    feature: add parsers for `avenue` and `clear`

---
+++ clear/parser.js
@@ -0,0 +1,53 @@
+const fs = require('fs/promises');
+const { EOL } = require('os');
+
+(async function main() {
+  try {
+    const input = await fs.readFile(__dirname + '/statement.csv', { encoding: 'utf8' });
+    const output = input
+      .split(EOL)
+      .filter((row, index) => {
+        return (
+          row && // remove qualquer linha vazia
+          index > 0 && // remove o cabeçalho
+          !row.includes('NOTA') && 
+          !row.includes('TED')
+        );
+      })
+      .map((row) => {
+        return row
+          // corrige nomenclaturas
+          .replace('JUROS S/CAPITAL', 'JCP;')
+          .replace('RENDIMENTO', 'Rendimentos;')
+          .replace('DIVIDENDOS', 'Dividendos;')
+          .replace('PAPEL', '')
+
+          // separa colunas
+          .replace(
+            /([0-9\/]+);([0-9\/]+);([0-9\,]+);(\w+);\s+(\d+)\s+([\w]+);([\d\,\.]+)/,
+            (match, ...groups) => {
+              const [
+                dataOperacao,
+                dataLiquidacao,
+                valor,
+                evento,
+                quantidade,
+                ativo,
+                saldoFinal
+              ] = groups;
+              
+              const moeda = 'Real';
+
+              return `${dataOperacao};${ativo};${evento};?;${moeda};${valor}`;
+            }
+          )
+      })
+      .join(EOL);
+
+    console.log(`Entrada: ${EOL}${EOL}${input}`);
+    console.log(`Saída: (Linhas = ${output.split(EOL).length}) ${EOL}${EOL}${output}${EOL}`);
+
+  } catch (err) {
+    console.log(err);
+  }
+})();
