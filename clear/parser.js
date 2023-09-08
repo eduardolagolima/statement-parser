b123547c668758b91e12003effe8e4bdef6cb651
Eduardo Lago Lima <eduardolagolima@gmail.com>
2023-09-08 17:13:00 -0300

    feature: make some improves (#2)

--- clear/parser.js
+++ clear/parser.js
@@ -1,52 +1,51 @@
-const fs = require('fs/promises');
-const { EOL } = require('os');
+const fs = require("fs/promises");
+const { EOL } = require("os");
 
 (async function main() {
   try {
-    const input = await fs.readFile(__dirname + '/statement.csv', { encoding: 'utf8' });
+    const input = await fs.readFile(__dirname + "/statement.csv", {
+      encoding: "utf8",
+    });
     const output = input
       .split(EOL)
       .filter((row, index) => {
         return (
           row && // remove qualquer linha vazia
           index > 0 && // remove o cabeçalho
-          !row.includes('NOTA') && 
-          !row.includes('TED')
+          !row.includes("NOTA") &&
+          !row.includes("TED")
         );
       })
       .map((row) => {
-        return row
+        const modifiedRow = row
           // corrige nomenclaturas
-          .replace('JUROS S/CAPITAL', 'JCP;')
-          .replace('RENDIMENTO', 'Rendimentos;')
-          .replace('DIVIDENDOS', 'Dividendos;')
-          .replace('PAPEL', '')
+          .replace("JUROS S/CAPITAL", "JCP")
+          .replace("RENDIMENTO", "Rendimentos")
+          .replace("DIVIDENDOS", "Dividendos")
+          .replace("CRÉDITO FRAÇÕES", "Crédito de frações")
 
-          // separa colunas
-          .replace(
-            /([0-9\/]+);([0-9\/]+);([0-9\,]+);(\w+);\s+(\d+)\s+([\w]+);([\d\,\.]+)/,
-            (match, ...groups) => {
-              const [
-                dataOperacao,
-                dataLiquidacao,
-                valor,
-                evento,
-                quantidade,
-                ativo,
-                saldoFinal
-              ] = groups;
-              
-              const moeda = 'Real';
+          // remove informação desnecessária dos FIIS
+          .replace("PAPEL", "")
 
-              return `${dataOperacao};${ativo};${evento};?;${moeda};${valor}`;
-            }
-          )
+          // remove a quantidade e substitui por um ponto e vírgula
+          .replace(/\s+\d*\s+/, ";");
+
+        const [dataOperacao, dataLiquidacao, valor, evento, ativo, saldoFinal] =
+          modifiedRow.split(";");
+
+        const tipo = "";
+        const moeda = "Real";
+
+        return `${dataOperacao};${ativo};${evento};${tipo};${moeda};${valor}`;
       })
       .join(EOL);
 
     console.log(`Entrada: ${EOL}${EOL}${input}`);
-    console.log(`Saída: (Linhas = ${output.split(EOL).length}) ${EOL}${EOL}${output}${EOL}`);
-
+    console.log(
+      `Saída: (Linhas = ${
+        output.split(EOL).length
+      }) ${EOL}${EOL}${output}${EOL}`
+    );
   } catch (err) {
     console.log(err);
   }
